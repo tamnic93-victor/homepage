106094035319f1680dbf0e33ec20531b4ce90b5b
Victorjeong <tamnic93@pantheonx.io>
2020-12-17 19:35:53 +0900

    design fix

--- src/App.tsx
+++ src/App.tsx
@@ -8,15 +8,26 @@ import './App.scss';
  */
 class App extends React.Component<any, any> {
   render = (): JSX.Element => (
-    <div className="contents">
-      <h1>
-        <span className="bc"> BC</span>&nbsp;
-        <span className="labs">LABS</span>
-      </h1>
-      <h2>
-        A universal access to digital asset management<br />
-        for anyone, anytime, and anywhere.
-      </h2>
+    <div>
+      <section className="section1">
+        <div className="textlogo">
+          <span className="bc">BC</span>
+          <span className="labs">Labs</span>
+        </div>
+        <div className="maintitle">
+          <span className="slogan1">Universal Access to
+          Digital Asset Management</span><br></br>
+          <span className="slogan2">“ for Anyone, Anytime, Anywhere ”</span>
+        </div>
+        <div className="subtitle">
+          <span className="whatwedo">
+          With a team specializing in fintech, blockchain
+          and data analytics, we seek to harness the cutting-edge
+          innovations and address the growing
+          technology gap in the global financial industry.
+          </span>
+        </div>
+      </section>
     </div>
   );
 };

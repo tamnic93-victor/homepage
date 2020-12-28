d3261971f55c5b4b8248e34e6157bbb32ba0cb91
Victorjeong <tamnic93@pantheonx.io>
2020-12-28 19:12:13 +0900

    divide section

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import './App.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.scss';
 import {Col, Container, Navbar, NavbarBrand, Row} from 'react-bootstrap';
+import logo from './images/lg_bclabs.svg';
 
 /**
  * App
@@ -9,37 +10,60 @@ import {Col, Container, Navbar, NavbarBrand, Row} from 'react-bootstrap';
  */
 class App extends React.Component<any, any> {
   render = (): JSX.Element => (
-    <Container fluid>
-      <Navbar>
-        <NavbarBrand>
-          <img
-            src="./images/lg_bclabs.png"
-            width="95"
-            height="36"
-            className="d-inline-block align-top"
-            alt="BCLabs"
-          />
-        </NavbarBrand>
-      </Navbar>
-      <Row className="justify-content-center my-5">
-        <Col xs={10} md={8} lg={8} xl={6}>
-          <div className="title">
-            Universal Access to Digital Asset Management<br/>
-            “for Anyone, Anytime, Anywhere ”
-          </div>
-        </Col>
-      </Row>
-      <Row className="justify-content-center my-3">
-        <Col xs={10} md={8} lg={8} xl={4}>
-          <p className="slogan">
-            With a team specializing in fintech, blockchain and data
-            analytics, we seek to harness the cutting-edge innovations
-            and address the growing technology gap in the global
-            financial industry.
-          </p>
-        </Col>
-      </Row>
-    </Container>
+    <div>
+      <Container id="section1" fluid>
+        <Navbar className='logo'>
+          <NavbarBrand>
+            <img
+              src={logo}
+              alt="BCLabs"
+            />
+          </NavbarBrand>
+        </Navbar>
+        <Row>
+          <Col>
+            <div className="mainphrase">
+              <h1 className="title1">
+                Universal Access to Digital Asset Management
+              </h1>
+              <h2 className="title2">
+                “for Anyone, Anytime, Anywhere”
+              </h2>
+            </div>
+          </Col>
+        </Row>
+        <Row className="justify-content-center">
+          <Col xs={10} md={8} lg={8} xl={6}>
+            <p className="slogan">
+              With a team specializing in fintech, blockchain and data
+              analytics, we seek to harness the cutting-edge innovations
+              and address the growing technology gap in the global
+              financial industry.
+            </p>
+          </Col>
+        </Row>
+      </Container>
+      <Container id="section2" fluid>
+        <Row>
+          <h3>What We Do</h3>
+        </Row>
+      </Container>
+      <Container id="section3" fluid>
+        <Row>
+          <h3>Company History</h3>
+        </Row>
+      </Container>
+      <Container id="section4" fluid>
+        <Row>
+          <h3>Partners</h3>
+        </Row>
+      </Container>
+      <Container id="section5" fluid>
+        <Row>
+          <h3>Copyright © 2020 bclabs Inc. All right reserved.</h3>
+        </Row>
+      </Container>
+    </div>
   );
 };
 

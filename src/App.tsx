7d3e1d6c26ccce0518337d4e386d293ce04b811e
Victorjeong <tamnic93@pantheonx.io>
2020-12-28 16:38:38 +0900

    이미지문제

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import './App.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Col, Container, Row} from 'react-bootstrap';
-
+import {Col, Container, Navbar, NavbarBrand, Row} from 'react-bootstrap';
 
 /**
  * App
@@ -10,25 +9,33 @@ import {Col, Container, Row} from 'react-bootstrap';
  */
 class App extends React.Component<any, any> {
   render = (): JSX.Element => (
-    <Container style={{backgroundImage: 'url(./images/topimage.png)'}} fluid>
-      <Row>
-        <Col className='textlogo'>
-          <span className="bc"><b>BC</b></span>
-          <span className="labs"><b>Labs</b></span>
+    <Container fluid>
+      <Navbar>
+        <NavbarBrand>
+          <img
+            src="./images/lg_bclabs.png"
+            width="95"
+            height="36"
+            className="d-inline-block align-top"
+            alt="BCLabs"
+          />
+        </NavbarBrand>
+      </Navbar>
+      <Row className="justify-content-center my-5">
+        <Col xs={10} md={8} lg={8} xl={6}>
+          <div className="title">
+            Universal Access to Digital Asset Management<br/>
+            “for Anyone, Anytime, Anywhere ”
+          </div>
         </Col>
       </Row>
-      <Row className="justify-content-center">
-        <Col xs={10} md={10} lg={10}>
-          <div className="title">Universal Access to Digital
-Asset Management<br></br>“ for Anyone, Anytime, Anywhere ”</div>
-        </Col>
-      </Row>
-      <Row className="justify-content-center">
-        <Col xs={10} md={10} lg={10}>
-          <p className="slogan">With a team specializing in fintech,
-blockchain and data analytics, we seek to harness the
-cutting-edge innovations and address the growing
-technology gap in the global financial industry.
+      <Row className="justify-content-center my-3">
+        <Col xs={10} md={8} lg={8} xl={4}>
+          <p className="slogan">
+            With a team specializing in fintech, blockchain and data
+            analytics, we seek to harness the cutting-edge innovations
+            and address the growing technology gap in the global
+            financial industry.
           </p>
         </Col>
       </Row>

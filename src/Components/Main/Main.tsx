5580dc62b5d842ebd01370bdf6d3998b8c259398
정관용 <tamnic93@gmail.com>
2021-01-07 00:13:12 +0900

    main complete

--- src/Components/Main/Main.tsx
+++ src/Components/Main/Main.tsx
@@ -18,8 +18,8 @@ class Main extends React.Component<any, any> {
         </span>
       </Navbar>
       <hr className="logodiv"/>
-      <Row className="vh-100 justify-content-center align-item-center">
-        <Col xl={8} className="text-center">
+      <Row className="vh-100 justify-content-center align-items-center">
+        <Col sm={10} md={10} lg={10} xl={8} className="text-center">
           <h1
             className="mb-5 maintitle"
             data-aos="fade"
@@ -40,12 +40,11 @@ class Main extends React.Component<any, any> {
             data-aos-once="false"
           >
             With a team specializing in fintech, blockchain and data
-            analytics, we seek to<br/>harness the cutting-edge innovations
-            and address the growing technology<br/>gap in the global
+            analytics, we seek to harness the cutting-edge innovations
+            and address the growing technology gap in the global
             financial industry.
           </p>
           <div
-            className="mainshape"
             data-aos="fade-up"
             data-aos-delay="800"
             data-aos-duration="2000"
@@ -55,6 +54,7 @@ class Main extends React.Component<any, any> {
             <img src={r1} alt="shape" />
           </div>
           <div
+            className="mainshape"
             data-aos="fade-down"
             data-aos-delay="800"
             data-aos-duration="2000"
